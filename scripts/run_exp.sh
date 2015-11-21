c46d39fd2036dbec1d7bab212e86d5e3ba2e62f0
Ruben Vereecken <rubenvereecken@gmail.com>
2015-11-21 16:57:31 +0100

    Worked on automation script

--- scripts/run_exp.sh
+++ scripts/run_exp.sh
@@ -1,38 +1,24 @@
 #!/bin/sh
-# Submit the job to all.q
-#$ -clear
-#$ -q all.q
-# use the current working directory for input and output
-#$ -cwd
-# export the current $PATH variable for this job
-#$ -V
-#$ -S /bin/bash
-# Set a custom job name
-#$ -N alejob
-#$ -l virtual_free=1G
-# stdout and stderr output
-#$ -e $HOME/Logs/alejob.err
-#$ -o $HOME/Logs/alejob.log
-#separate threads for processes
-#$ -pe mt 4
-#
 #set various paths
-RLDIR=$HOME/Documents/Code/PyALE/src
-ALEDIR=$HOME/Documents/Projects/ALE/ale5
-LOGDIR=$HOME/Logs
+SCRIPTDIR="$(cd "$(dirname "$0")" && pwd)"
+BASEDIR="${SCRIPTDIR}/.."
+RLDIR="${BASEDIR}/src"
+LOGDIR="${BASEDIR}/logs"
+ROMDIR="${BASEDIR}/roms"
+PYTHON="$HOME/anaconda/bin/python"
 
 #Experimental configuration
-EXP_NAME="sarsa"
-EXPERIMENT="generic_experiment.py"
-EXPERIMENT_OPTIONS="--maxsteps 18000  --numeps 5500"
+EXP_NAME="sarsa_RAM"
+EXPERIMENT="exp/generic_experiment.py"
+EXPERIMENT_OPTIONS="--maxsteps 2000  --numeps 100 --numtrials 3"
 AGENT="agents/ALESarsaAgent.py"
-AGENT_OPTIONS='--eps 0.05 --lambda_ 0.9 --alpha 0.5'
-ALE_OPTIONS="-game_controller rlglue  -frame_skip 5"
+AGENT_OPTIONS='--eps 0.05 --lambda 0.5 --alpha 0.1 --features RAM --actions 0 1 3 4'
+ALE_OPTIONS="-game_controller rlglue  -frame_skip 30 -repeat_action_probability 0.0"
 GAME="space_invaders.bin"
 
+echo "Writing logs to ${LOGDIR}"
+
 #######Imports#################
-source $HOME/.bash_profile
-module add java
 export PYTHONPATH=$RLDIR:$PYTHONPATH
 export RLGLUE_PORT=1028
 ###############################
@@ -43,10 +29,8 @@ cd $RLDIR
 #start rlglue
 rl_glue&
 #run agent
-python $AGENT $AGENT_OPTIONS --savepath "$LOGDIR/$EXP_NAME" > $LOGDIR/agent-$EXP_NAME.log 2>&1 &
+$PYTHON $AGENT $AGENT_OPTIONS --savepath "$LOGDIR/$EXP_NAME" > $LOGDIR/agent-$EXP_NAME.log 2>&1 &
 #run experiment
-cd exp
-python $EXPERIMENT $EXPERIMENT_OPTIONS  >> $LOGDIR/exp-$EXP_NAME.log 2>&1 &
+$PYTHON $EXPERIMENT $EXPERIMENT_OPTIONS >> $LOGDIR/exp-$EXP_NAME.log 2>&1 &
 #run environment (no '&' or job quits!)
-cd $ALEDIR
-./ale $ALE_OPTIONS roms/$GAME > $LOGDIR/ale-$EXP_NAME.log 2>&1
+ale $ALE_OPTIONS $ROMDIR/$GAME > $LOGDIR/ale-$EXP_NAME.log 2>&1
