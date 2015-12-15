5cb80b232f09b89baa0cfafe27fb86a6d3812640
Ruben Vereecken <rubenvereecken@gmail.com>
2015-12-15 11:30:21 +0100

    Parameter tweak

--- scripts/run_exp.sh
+++ scripts/run_exp.sh
@@ -10,7 +10,7 @@ PYTHON="$HOME/anaconda/bin/python"
 #Experimental configuration
 EXP_NAME="sarsa_RAM"
 EXPERIMENT="exp/generic_experiment.py"
-EXPERIMENT_OPTIONS="--maxsteps 2000  --numeps 2000 --numtrials 5"
+EXPERIMENT_OPTIONS="--maxsteps 2000  --numeps 3000 --numtrials 5"
 AGENT="agents/ALESarsaAgent.py"
 AGENT_OPTIONS='--eps 0.05 --lambda 0.5 --alpha 0.1 --features RAM --actions 0 1 3 4'
 ALE_OPTIONS="-game_controller rlglue  -frame_skip 30 -repeat_action_probability 0.0"
