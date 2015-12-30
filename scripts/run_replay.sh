750b16951daae04727686c64cd07aeedec018fc1
Ruben Vereecken <rubenvereecken@gmail.com>
2015-12-30 23:20:24 +0100

    Allowed tweakking of replay times

--- scripts/run_replay.sh
+++ scripts/run_replay.sh
@@ -3,14 +3,14 @@
 #Experimental configuration
 RANDOM_SEED=${RANDOM_SEED:-"42"}
 DISABLE_TRACES=${DISABLE_TRACES:-""}
-
+REPLAY_TIMES=${REPLAY_TIMES:-3}
 
 
 EXP_NAME="replay_sarsa"
 EXPERIMENT="exp/generic_experiment.py"
-EXPERIMENT_OPTIONS="--maxsteps 2000  --numeps 1 --numtrials 1"
+EXPERIMENT_OPTIONS="--maxsteps 2000  --numeps 3000 --numtrials 5"
 AGENT="agents/ALEReplayAgent.py"
-AGENT_REPLAY_OPTIONS="--random_seed=${RANDOM_SEED} --replay_memory=5000 --replay_times=3 --replay_frequency=100"
+AGENT_REPLAY_OPTIONS="--random_seed=${RANDOM_SEED} --replay_memory=5000 --replay_times=${REPLAY_TIMES} --replay_frequency=100"
 AGENT_OPTIONS="--eps 0.05 --lambda 0.5 --alpha 0.1 --actions 0 1 3 4 ${AGENT_REPLAY_OPTIONS}"
 ALE_OPTIONS="-game_controller rlglue  -frame_skip 30 -repeat_action_probability 0.0 -random_seed ${RANDOM_SEED}"
 GAME="space_invaders"
